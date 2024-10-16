acc0fae5d628c9ff26fb10ef30595c5f3ccc2463
himynameismartin <himynameismartin@gmail.com>
2024-10-16 08:08:25 +0200

    feat: add support for component-specific themes to themeable component group creator

--- src/factories/createThemeableComponentGroup.stories.tsx
+++ src/factories/createThemeableComponentGroup.stories.tsx
@@ -6,16 +6,40 @@ import { ThemeContextProvider, ThemeProvider } from '../contexts/ThemeProvider';
 import Element from '../components/Element';
 import createThemeableComponentGroup from './createThemeableComponentGroup'
 
+const THEME_OPTIONS = ['vanilla', 'pistachio'];
+
 const themes = {
   vanilla: {
     components: {
       default: {
         color: '#F3E5AB'
       }
+    },
+    headings: {
+      default: {
+        color: '#F3E5AB'
+      },
+      H1: {
+        fontSize: '32px'
+      },
+      H2: {
+        fontSize: '24px'
+      },
+      H3: {
+        fontSize: '20px'
+      },
+      H4: {
+        fontSize: '16px'
+      }
     }
   },
   pistachio: {
     components: {
+      default: {
+        color: '#93C572'
+      }
+    },
+    headings: {
       default: {
         color: '#93C572'
       }
@@ -28,17 +52,23 @@ const StyledComponent = styled(Element)``;
 const createThemeableComponent = createThemeableComponentGroup({ name: 'components' });
 const ThemeableComponent = createThemeableComponent({ as: 'div', component: StyledComponent });
 
-const THEME_OPTIONS = ['vanilla', 'pistachio'];
-
-const ThemeableComponentApp: React.FC<{ theme: (typeof THEME_OPTIONS)[number] }> = ({ theme }) => (
+const ThemeableComponentApp: React.FC<{
+  theme: (typeof THEME_OPTIONS)[number], children: React.ReactNode
+}> = ({ theme, children }) => (
   <ThemeContextProvider themes={themes}>
     <ThemeProvider name="vanilla">
       <ThemeProvider name={theme}>
-        <ThemeableComponent>theinium</ThemeableComponent>
+        {children}
       </ThemeProvider>
     </ThemeProvider>
   </ThemeContextProvider>
 );
+
+const createThemeableHeading = createThemeableComponentGroup({ name: 'headings' });
+const ThemeableH1 = createThemeableHeading({ as: 'h1', component: StyledComponent, name: 'H1' });
+const ThemeableH2 = createThemeableHeading({ as: 'h2', component: StyledComponent, name: 'H2' });
+const ThemeableH3 = createThemeableHeading({ as: 'h3', component: StyledComponent, name: 'H3' });
+const ThemeableH4 = createThemeableHeading({ as: 'h4', component: StyledComponent, name: 'H4' });
 
 const meta: Meta<typeof ThemeableComponentApp> = {
   title: 'createThemeableComponentGroup',
@@ -62,4 +92,32 @@ export default meta;
 
 type Story = StoryObj<typeof ThemeableComponentApp>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    children: <ThemeableComponent>theinium</ThemeableComponent>,
+  },
+};
+
+export const H1: Story = {
+  args: {
+    children: <ThemeableH1>theinium</ThemeableH1>,
+  },
+};
+
+export const H2: Story = {
+  args: {
+    children: <ThemeableH2>theinium</ThemeableH2>,
+  },
+};
+
+export const H3: Story = {
+  args: {
+    children: <ThemeableH3>theinium</ThemeableH3>,
+  },
+};
+
+export const H4: Story = {
+  args: {
+    children: <ThemeableH4>theinium</ThemeableH4>,
+  },
+};
--- src/factories/createThemeableComponentGroup.tsx
+++ src/factories/createThemeableComponentGroup.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from '@emotion/react';
+import { defaultsDeep } from 'lodash';
 import { getOr } from 'lodash/fp';
 import React, { ElementType, ComponentType } from 'react';
 import { Element } from '../components';
@@ -8,21 +9,27 @@ type ThemeableComponentProps = Record<string, any>;
 
 type CreateThemeableComponentGroup = {
   (params: { name: string, component?: ElementType }): (
-    options?: { as?: ElementType, component?: ElementType }
+    options?: { as?: ElementType, component?: ElementType, name?: string }
   ) => ComponentType<ThemeableComponentProps>;
 };
 
 const createThemeableComponentGroup: CreateThemeableComponentGroup = ({
-  name,
+  name: groupName,
   component: groupComponent = Element
-}) => ({ as = DEFAULT_HTML_TAG, component = Element } = {}) => {
+}) => ({ as = DEFAULT_HTML_TAG, component = Element, name = '' } = {}) => {
   const ThemeableComponent: React.FC<ThemeableComponentProps> = ({ ...rest }) => {
     const theme = useTheme();
     const Tag = component || groupComponent;
 
-    const groupTheme = getOr({}, `${name}.default`, theme);
+    const groupTheme = getOr({}, `${groupName}.default`, theme);
+    const componentTheme = getOr({}, `${groupName}.${name}`, theme);
 
-    return <Tag as={as} {...groupTheme} {...rest} />;
+    const mergedTheme = defaultsDeep(
+      componentTheme,
+      groupTheme,
+    )
+
+    return <Tag as={as} {...mergedTheme} {...rest} />;
   };
 
   return ThemeableComponent;
